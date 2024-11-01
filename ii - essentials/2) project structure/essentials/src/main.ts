5d0106712b3b40f1ae2a3f6c27d3e609f589878a
Ela <micaelafrancisco13@outlook.com>
2024-11-01 12:39:46 +0800

    angular essentials - 11
    add notes for understanding components and how content ends up on screen

--- ii - essentials/2) project structure/essentials/src/main.ts
+++ ii - essentials/2) project structure/essentials/src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// the ./app/app.component points to the src/app/app.component.ts file
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
