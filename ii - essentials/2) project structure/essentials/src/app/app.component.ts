a883da38438b136fc975578b726682321d88b6f2
Ela <micaelafrancisco13@outlook.com>
2024-11-01 12:52:02 +0800

    angular essentials - 12 to 15
    add notes for first custom component

--- ii - essentials/2) project structure/essentials/src/app/app.component.ts
+++ ii - essentials/2) project structure/essentials/src/app/app.component.ts
@@ -1,32 +1,14 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {HeaderComponent} from './header.component';
 
-/*
-* Decorators in TypeScript are a form of meta-programming that allows you to attach custom metadata or
-* alter the behavior of classes, methods, properties, or parameters.
-* Decorators allow you to add, override, or extend the functionality of classes without modifying the
-* actual code within them.
-* */
 @Component({
-  // selector Determines how you’ll instantiate the component in HTML, like a custom tag or attribute.
-  // <app-root></app-root>
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-
-  // templateUrl Points to an external HTML file that holds the component's view (structure), keeping
-  // HTML separate from TypeScript for readability.
+  imports: [RouterOutlet, HeaderComponent], // register the HeaderComponent
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'essentials';
 }
-
-// the exported AppComponent is imported to main.ts
-// import { AppComponent } from './app/app.component';
-
-/*
-* The HTML markup defined in the templateUrl (or template, if you’re using an inline template) will be
-* displayed wherever you use the component’s selector in your application.
-* */
