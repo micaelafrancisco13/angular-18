35f020b48bba965bb9dc1d34feb83ea6d1c64896
Ela <micaelafrancisco13@outlook.com>
2024-11-01 13:31:11 +0800

    angular essentials - 17
    add notes for creating components using the Angular CLI

--- ii - essentials/2) project structure/essentials/src/app/app.component.ts
+++ ii - essentials/2) project structure/essentials/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { RouterOutlet } from '@angular/router';
 import {Component} from '@angular/core';
-import {HeaderComponent} from './header.component';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
